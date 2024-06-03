19d0bb56710ca26660c051f156c2e61ab58baf1a
SPL-HMRI <splhmri@houstonmethodist.org>
2024-06-03 13:09:24 -0500

    Add files via upload

---
+++ segmentation3d/core/fine_cpp.py
@@ -0,0 +1,10 @@
+import os
+
+def extract_cpp()
+    path='E:\AAProgram\split_2022-current'
+    log_file = os.path.join(path, 'Output-Build.txt')
+
+
+
+if __name__ == '__main__':
+    extract_cpp()
